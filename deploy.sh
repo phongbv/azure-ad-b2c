49b92d871849fc6d2488ef4f6967084468567da8
Bùi Văn Phong <buivan.phong94@gmail.com>
2023-08-05 03:56:06 +0000

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+deploymentFolder=Deployment
+[ ! -d $deploymentFolder ] && mkdir $deploymentFolder
+rm -Rf $deploymentFolder/*
+cp *.xml $deploymentFolder/
+
+sed s/-/_/g appsettings.Development.json > $deploymentFolder/appsettings.json
+cd $deploymentFolder
+
+
+tenant=`jq -r .Environments[0].Tenant appsettings.json`
+identityAppId=`jq -r .Environments[0].PolicySettings.IdentityExperienceFrameworkAppId appsettings.json`
+proxyAppId=`jq -r .Environments[0].PolicySettings.ProxyIdentityExperienceFrameworkAppId appsettings.json`
+b2cClientId=`jq -r '.Environments[0].PolicySettings.b2c_extensions_app_ClientId' appsettings.json`
+b2cObjectId=`jq -r '.Environments[0].PolicySettings.b2c_extensions_app_ObjectId' appsettings.json`
+echo $b2cClientId
+
+sed -i s/\{Settings:Tenant\}/$tenant/ *.xml
+sed -i s/\{Settings:IdentityExperienceFrameworkAppId\}/$identityAppId/g *.xml 
+sed -i s/\{Settings:ProxyIdentityExperienceFrameworkAppId\}/$proxyAppId/g *.xml 
+sed -i s/\{Settings:b2c-extensions-app-ClientId\}/$b2cClientId/g *.xml 
+sed -i s/\{Settings:b2c-extensions-app-ObjectId\}/$b2cObjectId/g *.xml
+sed -i s/\{Settings:FacebookAppId\}//g *.xml
+sed -i s/\{Settings:GoogleAppId\}//g *.xml
+sed -i s/\{Settings:FacebookAppId\}//g *.xml
+sed -i s/\{Settings:FacebookAppId\}//g *.xml
+sed -i s/\{Settings:FacebookAppId\}//g *.xml
+
+
