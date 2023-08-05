583ddf122bc553b1f90ae6c44ef86a0a3baf0776
Bùi Văn Phong <buivan.phong94@gmail.com>
2023-08-05 04:21:33 +0000

    Fix deployment policy and remove logging

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@ deploymentFolder=Deployment
 rm -Rf $deploymentFolder/*
 cp *.xml $deploymentFolder/
 
-sed s/-/_/g appsettings.Development.json > $deploymentFolder/appsettings.json
+sed s/b2c-extensions-app-ClientId/b2c_extensions_app_ClientId/g appsettings.Development.json > $deploymentFolder/appsettings.json
 cd $deploymentFolder
+sed -i s/b2c-extensions-app-ObjectId/b2c_extensions_app_ObjectId/g appsettings.json
 
 
 tenant=`jq -r .Environments[0].Tenant appsettings.json`
@@ -20,8 +21,8 @@ sed -i s/\{Settings:IdentityExperienceFrameworkAppId\}/$identityAppId/g *.xml
 sed -i s/\{Settings:ProxyIdentityExperienceFrameworkAppId\}/$proxyAppId/g *.xml 
 sed -i s/\{Settings:b2c-extensions-app-ClientId\}/$b2cClientId/g *.xml 
 sed -i s/\{Settings:b2c-extensions-app-ObjectId\}/$b2cObjectId/g *.xml
-sed -i s/\{Settings:FacebookAppId\}//g *.xml
-sed -i s/\{Settings:GoogleAppId\}//g *.xml
+sed -i s/\{Settings:FacebookAppId\}/71263/g *.xml
+sed -i s/\{Settings:GoogleAppId\}/123123/g *.xml
 sed -i s/\{Settings:FacebookAppId\}//g *.xml
 sed -i s/\{Settings:FacebookAppId\}//g *.xml
 sed -i s/\{Settings:FacebookAppId\}//g *.xml
